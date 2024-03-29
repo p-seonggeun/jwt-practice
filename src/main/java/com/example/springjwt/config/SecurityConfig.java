cb62ca4095d461c76f316df1c2510028adb51ee3
psg <1999byh@naver.com>
2024-03-29 22:02:42 +0900

    SecurityConfig 클래스 추가

---
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.springjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // Security를 통해 비밀번호를 해시로 암호화 시켜서 검증
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        // Form 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        // http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // 경로별 인가작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        // ** 제일 중요 **
+        // JWT 방식에서는 세션을 항상 STATELESS 상태로 관리
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
