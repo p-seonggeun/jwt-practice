2bcef968ac8388b107b190bdcd9c540617e30d74
psg <1999byh@naver.com>
2024-03-29 22:46:45 +0900

    로그인 필터 구현

--- src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -1,22 +1,35 @@
 package com.example.springjwt.config;
 
+import com.example.springjwt.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     // Security를 통해 비밀번호를 해시로 암호화 시켜서 검증
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -39,6 +52,11 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        // 원래 UsernamePasswordAuthenticationFilter 자리에 커스텀한 LoginFilter를 추가함
+        // authenticationManager 메서드의 인자는 생성자 주입을 받음
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         // ** 제일 중요 **
         // JWT 방식에서는 세션을 항상 STATELESS 상태로 관리
---
+++ src/main/java/com/example/springjwt/jwt/LoginFilter.java
@@ -0,0 +1,51 @@
+package com.example.springjwt.jwt;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * UsernamePasswordAuthenticationFilter를 커스텀 (FormLogin 방식을 disable 시켰기 때문에 기존 필터가 동작하지 않아서)
+ * Filter에 요청이 오면 attemptAuthentication 메서드가 호출되고
+ * request에서 username, password를 추출한뒤에 UsernamePasswordAuthenticationToken(DTO 역할)로 변환
+ * 변환한 후 검증을 위해 AuthenticationManager로 전달하면 AuthenticationManager에서 검증을 진행
+ * 검증 방법은 DB에서 회원정보를 땡겨온뒤 UserDetailsService를 통해 정보를 받고 검증
+ */
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)  throws AuthenticationException {
+        // 클라이언트 요청에서 username, password 추출
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        // 스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야함
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        // token에 담은 검증을 위한 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    // 로그인 성공시 실행하는 메서드 (여기서 JWT를 발급하면 됨)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    // 로그인 실패시 실행하는 메서드
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+}
