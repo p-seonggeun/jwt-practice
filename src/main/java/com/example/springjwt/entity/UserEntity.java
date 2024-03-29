a5968da69a833f6325002b8c40dc39564996f314
psg <1999byh@naver.com>
2024-03-29 22:14:09 +0900

    DB연결 및 Entity 작성

---
+++ src/main/java/com/example/springjwt/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package com.example.springjwt.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+}
